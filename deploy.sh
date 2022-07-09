bb39eacc14fdf5226c5f8279442555ea035e25a0
Evgeny Zinoviev <me@ch1p.io>
2022-07-09 19:55:36 +0300

    deploy: change composer name

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ fi
 git reset --hard
 git pull origin master
 
-$PHP composer.phar install --no-dev --optimize-autoloader
+composer8.1 install --no-dev --optimize-autoloader
 $PHP prepare_static.php
 
 cp "$DEV_DIR/config-local.php" .
