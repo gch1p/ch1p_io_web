e12220dd63e9c91d87670c1ab413ba3d090c164d
Evgeny Zinoviev <me@ch1p.io>
2022-07-11 12:41:24 +0300

    js: add some comments

--- htdocs/js/common/30-static-manager.js
+++ htdocs/js/common/30-static-manager.js
@@ -1,12 +1,28 @@
 var StaticManager = {
+    /**
+     * @type {string[]}
+     */
     loadedStyles: [],
+
+    /**
+     * @type {object}
+     */
     versions: {},
 
+    /**
+     * @param {string[]} loadedStyles
+     * @param {object} versions
+     */
     init: function(loadedStyles, versions) {
         this.loadedStyles = loadedStyles;
         this.versions = versions;
     },
 
+    /**
+     * @param {string} name
+     * @param {string} theme
+     * @param {function} callback
+     */
     loadStyle: function(name, theme, callback) {
         var url, id;
         if (!window.appConfig.devMode) {
